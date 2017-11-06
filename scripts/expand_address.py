d726a7a73ef1f1a51a6dafbe3a46b53d505fbb9c
jmd862000 <>
2017-11-06 12:21:19 -0500

    Return IP Hosts given a subnet

--- scripts/expand_address.py
+++ scripts/expand_address.py
@@ -1,6 +1,35 @@
 #!/usr/bin/env python3
-# expand_address.py
+# iptools.py
 import ipaddress
+import sys
+import argparse
 
-def expand_address(ip:str(),version:int()):
-    pass
+parser = argparse.ArgumentParser()
+parser.add_argument("--network", help = "CIDR Address to expand", metavar="NET",required=True)
+parser.add_argument("--version", help = "IP Version (e.g. 4 or 6)", metavar="VER", default=4, choices=['4','6'])
+
+def list_addresses(ip:str(),version:int()) -> list():	
+    result = []
+    if version == 4:
+        i = ipaddress.IPv4Interface(ip)
+        if i.network.num_addresses>65536:
+            start = i.network.network_address+1
+            end = i.network.broadcast_address-1
+            return [start.exploded,end.exploded]
+    elif version == 6:
+        i = ipaddress.IPv6Interface(ip)
+        start = i.network.network_address+1
+        end = i.network.broadcast_address-1
+        return [start.exploded,end.exploded]
+    else:
+        raise ipaddress.AddressValueError("Invalid IP Version")
+    result = [sIP.exploded for sIP in i.network.hosts()]
+    return result	
+
+options = parser.parse_args()
+
+if options.network:
+    for ip in list_addresses(options.network,int(options.version)):
+        print(ip)
+else:
+    parser.print_help()
